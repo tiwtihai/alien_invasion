e433319818bfd34baf9a56bb79796332beec0534
tiwtihai <tiwtihai@sina.com>
2019-11-20 13:26:42 +0800

    在linux上修改

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,7 +3,7 @@ import pygame
 import game_functions as gf
 from settings import Settings
 from ship import Ship
-
+from statusbar import StausBar
 
 def run_game():
     pygame.init()
@@ -11,10 +11,11 @@ def run_game():
     screen = pygame.display.set_mode((game_settings.screen_width, game_settings.screnn_height))
     pygame.display.set_caption('Alien Invasion')
     ship = Ship(screen, game_settings.ship_speed_factor)
-
+    statusbar=StausBar(screen)
     while True:
         gf.check_events(ship)
         ship.update()
+        statusbar.printtext()
         gf.update_screen(game_settings, screen, ship)
 
 
--- statusbar.py
+++ statusbar.py
@@ -5,7 +5,7 @@ class StausBar:
     def __init__(self, screen):
         self.screen = screen
         self.textfontobj = pygame.font.SysFont('SimHei', 32)
-        self.textsurfaceobj = self.textfontobj.render('test', True, GREEN, BLUE)
+        self.textsurfaceobj = self.textfontobj.render('test', True,(0,0,255))
         self.textrectobj = self.textsurfaceobj.get_rect()
         self.textrectobj.center = (200, 150)
 
